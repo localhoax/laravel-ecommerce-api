336ad42fb110f161f173db109522c5977e9f629f
frozen4code <bhpndrsingh306@gmail.com>
2018-10-12 10:23:26 +0530

    CORS

--- config/app.php
+++ config/app.php
@@ -172,7 +172,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        // CORS
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
