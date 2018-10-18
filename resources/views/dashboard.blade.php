f8d16be6138bdaf36f3999575ce62bb10ad8069d
root <bhpndrsingh306@gmail.com>
2018-10-18 20:21:37 +0000

    Updated everything? idk

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Smart Retail API</title>
+        <title>Home Shop</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <!-- Styles -->
